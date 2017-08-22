8234dffa47860b2c54674fde713d5245422c460f
Chi <chulinguy@gmail.com>
2017-08-22 03:01:36 -0700

    added bounce

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+  $('.icon').hover( function() {
+    $(this).attr('bounce', true);
+    bounce($(this));
+    }, function() { 
+      $(this).attr('bounce', false);
+    }
+  )
+
+function bounce($elem) {
+    $elem.effect('bounce', { times: 1, distance: 10 }, 1200, ()=>{
+      if ($elem.attr('bounce') =='true') bounce($elem);
+      else {
+        $elem.stop()
+      }
+    });
+}
