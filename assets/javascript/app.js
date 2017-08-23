5f08a643fad38e8c63c4d9516099f1be23ac3e0d
Chi <chulinguy@gmail.com>
2017-08-23 02:31:44 -0700

    contact page completely working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+setTimeout(()=>{
+  $("#SkypeButton_Call_chulinguy_1").find('img').addClass('icon')
   $('.icon').hover( function() {
     $(this).attr('bounce', true);
     bounce($(this));
@@ -6,6 +8,8 @@
       $(this).stop();
     }
   )
+}, 500);
+
 
 function bounce($elem) {
     $elem.effect('bounce', { times: 1, distance: 10 }, 1200, ()=>{
